4d991867b05fbe188176b518af0071cd024959e1
Moses Byanyuma <mbyanyuma108@gmail.com>
2020-05-04 03:26:20 -0400

    Creating the real Pie and Category repositories

---
+++ Models/PieRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PizzaHub.Models
+{
+    public class PieRepository : IPieRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public PieRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Pie> AllPies
+        {
+            get { return _appDbContext.Pies.Include(incl => incl.Category); }
+        }
+
+        public IEnumerable<Pie> PiesOfTheWeek
+        {
+            get { return _appDbContext.Pies.Include(incl => incl.Category).Where(p => p.IsPieOfTheWeek); }
+        }
+
+        public Pie GetPieById(int pieId)
+        {
+            return _appDbContext.Pies.FirstOrDefault(p => p.PieId == pieId);
+        }
+    }
+}
